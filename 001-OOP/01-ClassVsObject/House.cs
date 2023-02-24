28a66e5bbbc542e49fcaefb0fd4e1aa087b45007
ROBSON JUNIOR <robsonbrazjunior@gmail.com>
2023-02-24 12:58:20 -0300

    Class and Object example

---
+++ 001-OOP/01-ClassVsObject/House.cs
@@ -0,0 +1,10 @@
+﻿namespace _00P._01_ClassVsObject
+{
+	internal class House
+	{
+		public int Size { get; set; }
+		public int Floors { get; set; }
+		public decimal Value { get; set; }
+		public int ParkingNumber { get; set; }
+	}
+}
---
+++ 001-OOP/01-ClassVsObject/Object.cs
@@ -0,0 +1,16 @@
+﻿namespace _00P._01_ClassVsObject
+{
+	internal class Object
+	{
+		internal Object()
+		{
+			var house = new House()
+			{
+				Size = 100,
+				Floors = 2,
+				Value = 1_000_000,
+				ParkingNumber = 3
+			};
+		}
+	}
+}
