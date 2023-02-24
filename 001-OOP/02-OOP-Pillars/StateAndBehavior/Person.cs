ceb69d802089264efe99eb7c39bf691494cbc163
ROBSON JUNIOR <robsonbrazjunior@gmail.com>
2023-02-24 13:10:01 -0300

    State and Behavior example

---
+++ 001-OOP/02-OOP-Pillars/StateAndBehavior/Person.cs
@@ -0,0 +1,21 @@
+﻿namespace _00P._02_OOP_Pillars.StateAndBehavior
+{
+	internal class Person
+	{
+		// State
+		public string Name { get; set; }
+		public DateTime Birthday { get; set; }
+
+		// Behavior
+		public int CalculateAge()
+		{
+			var today = DateTime.Now;
+			var age = today.Year - Birthday.Year;
+
+			if (today < Birthday.AddYears(age))
+				age--;
+
+			return age;
+		}
+	}
+}
