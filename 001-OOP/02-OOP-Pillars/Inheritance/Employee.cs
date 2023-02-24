88eb3eeef3a762e695fd382ae1ce0675dcdbc888
ROBSON JUNIOR <robsonbrazjunior@gmail.com>
2023-02-24 13:50:33 -0300

    Inheritance example

---
+++ 001-OOP/02-OOP-Pillars/Inheritance/Employee.cs
@@ -0,0 +1,10 @@
+﻿using _00P._02_OOP_Pillars.StateAndBehavior;
+
+namespace _00P._02_OOP_Pillars.Inheritance
+{
+	internal class Employee : Person
+	{
+		public DateTime AdmissionDate { get; set; }
+		public string Registry { get; set; }
+	}
+}
---
+++ 001-OOP/02-OOP-Pillars/Inheritance/Process.cs
@@ -0,0 +1,18 @@
+﻿namespace _00P._02_OOP_Pillars.Inheritance
+{
+	internal class Process
+	{
+		public void Execute()
+		{
+			var employee = new Employee()
+			{
+				Name = "Name of a Person",
+				Birthday = Convert.ToDateTime("1990/01/01"),
+				AdmissionDate = DateTime.Now,
+				Registry = "0123456"
+			};
+
+			employee.CalculateAge();
+		}
+	}
+}
