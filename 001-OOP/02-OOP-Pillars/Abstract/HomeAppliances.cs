c829d123af29fd2f37e2c9048a0ea6090f683d95
ROBSON JUNIOR <robsonbrazjunior@gmail.com>
2023-02-24 14:08:26 -0300

    Abstract example

---
+++ 001-OOP/02-OOP-Pillars/Abstract/HomeAppliances.cs
@@ -0,0 +1,17 @@
+﻿namespace _00P._02_OOP_Pillars.Abstract
+{
+	internal abstract class HomeAppliances
+	{
+		private readonly string _name;
+		private readonly int _voltage;
+
+		protected HomeAppliances(string name, int voltage)
+		{
+			_name = name;
+			_voltage = voltage;
+		}
+
+		internal abstract void TurnOn();
+		internal abstract void TurnOff();
+	}
+}
